912c69ab1ba2a0276010510a44c5a476e944497b
Luis Villalobos <villalobos23@gmail.com>
2019-01-16 18:52:26 -0800

    Day 4 lists and their kind were not so different

--- src/koans/03_lists.clj
+++ src/koans/03_lists.clj
@@ -6,40 +6,41 @@
   (= '(1 2 3 4 5) (list 1 2 3 4 5)) 
 
   "They are Clojure seqs (sequences), so they allow access to the first"
-  (= __ (first '(1 2 3 4 5))) ;https://clojure.org/reference/sequences
+  (= 1 (first '(1 2 3 4 5))) ;https://clojure.org/reference/sequences
 
   "As well as the rest"
-  (= __ (rest '(1 2 3 4 5)))
+  (= (list 2 3 4 5) (rest '(1 2 3 4 5))) ; rest gives you what is after the first element
 
   "Count your blessings"
-  (= __ (count '(dracula dooku chocula)))
+  (= 3 (count '(dracula dooku chocula)))
 
   "Before they are gone"
-  (= __ (count '()))
+  (= 0 (count '()))
 
   "The rest, when nothing is left, is empty"
-  (= __ (rest '(100)))
+  (= '() (rest '(100))) ; empty seq -> '()
 
   "Construction by adding an element to the front is easy"
-  (= __ (cons :a '(:b :c :d :e)))
+  (= '(:a :b :c :d :e) (cons :a '(:b :c :d :e))) ; :keywords have colons symbols quotes
 
   "Conjoining an element to a list isn't hard either"
-  (= __ (conj '(:a :b :c :d) :e))
+  (= '(:e :a :b :c :d) (conj '(:a :b :c :d) :e)) ; cons and conj apparently do the same but they vary depending on the type of the collection passed plus the arg quantity and order
+    ;https://stackoverflow.com/questions/3008411/clojure-consseq-vs-conjlist
 
   "You can use a list like a stack to get the first element"
-  (= __ (peek '(:a :b :c :d :e)))
+  (= (keyword "a") (peek '(:a :b :c :d :e)))
 
   "Or the others"
-  (= __ (pop '(:a :b :c :d :e)))
+  (= '(:b :c :d :e) (pop '(:a :b :c :d :e))) ;the symbol reserved keyword is not the same as the quote notation
 
   "But watch out if you try to pop nothing"
-  (= __ (try
+  (= "No dice!" (try
           (pop '())
           (catch IllegalStateException e
-            "No dice!")))
+            "No dice!"))) ;catch associates the rules after the expected exception description to be executed if it is the first match to be found
 
   "The rest of nothing isn't so strict"
-  (= __ (try
+  (= '() (try
           (rest '())
           (catch IllegalStateException e
             "No dice!"))))
